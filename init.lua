a788b2f1c202c179deaf45cb423b1bce73d098a7
swagger <1246318713@qq.com>
2023-07-01 03:28:44 +0800

    init

--- init.lua
+++ init.lua
@@ -1,7 +1,11 @@
+-- plugins manager
 require("plugins.plugins-setup")
+
+-- some options and mapping
 require("core.options")
 require("core.keymaps")
 
+-- plugins
 require("plugins/nvim-tree")
 require("plugins/treesitter")
 require("plugins/lsp")
